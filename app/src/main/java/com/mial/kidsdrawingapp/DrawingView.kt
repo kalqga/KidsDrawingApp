5c4b692a3255db888479cc86add0a4c67bc133d5
mialskywalker <kalqga123@gmail.com>
2021-11-14 13:27:15 +0200

    Preparing the BrushSizeSelector and How To Use DisplayMetrics

--- app/src/main/java/com/mial/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/mial/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.mial.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -85,8 +86,15 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
             else -> return false
         }
         invalidate()
-
         return true
+    }
+
+    fun setSizeForBrush(newSize : Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
