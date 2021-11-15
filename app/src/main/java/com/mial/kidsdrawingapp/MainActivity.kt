ed0094a333226adddc0d9e8c940b5fff8b11ef7e
mialskywalker <kalqga123@gmail.com>
2021-11-15 18:38:14 +0200

    Adding the Color selection

--- app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.mial.kidsdrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
@@ -61,5 +63,25 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
 
     }
+
+    fun paintClicked(view: View){
+        if (view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+
+        }
+    }
+
 
 }
