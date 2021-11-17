6dff09c497ce39fcc51786c6268ecb2f766f7d3b
mialskywalker <kalqga123@gmail.com>
2021-11-17 12:35:40 +0200

    Adding a provider - path and a image sandwich maker to our app

--- app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -167,6 +170,20 @@ class MainActivity : AppCompatActivity() {
                     dialog.dismiss()
                 }
         builder.create().show()
+    }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+        return returnedBitmap
     }
 
 
