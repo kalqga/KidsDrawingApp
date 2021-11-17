0390aba1cb1619ee4353fb9d810f77e17e91f01d
mialskywalker <kalqga123@gmail.com>
2021-11-17 13:17:34 +0200

    Adding a share feature to share the image via-email-whatsapp etc.

--- app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/mial/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -234,6 +235,7 @@ class MainActivity : AppCompatActivity() {
                                 "File saved successfully: $result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         }else{
                             Toast.makeText(
                                 this@MainActivity,
@@ -266,7 +268,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     // SHARE
-    //private fun shareImage
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+        }
+    }
 
 
 }
